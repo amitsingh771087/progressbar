a0a507e8f81d15b8002c31e45a957e1e84965e65
amitsingh771087 <amitsingh77108792@gmail.com>
2024-03-24 22:12:10 +0530

    optimized the code

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 // import "./style.css";
+import{ MAX , MIN } from './components/constants'
 
 const ProgressBar = ({value=0}) => {
     const[percentage, setpercentage]=  useState(value)
     useEffect(()=>{
-        setpercentage(Math.min(100,Math.max(value,0)))
+        setpercentage(Math.min(MAX,Math.max(value,MIN)))
     },[value])
   return (
     <div className='progress'>
         <span 
             style={{color:percentage>49 ? "White":"black"}}
         >{percentage.toFixed()}%</span>
-        <div style={{width: `${percentage}%`}} role='progressbar' aria-valuemax={100} aria-valuemin={0} aria-valuenow={percentage.toFixed()}/>
+        <div style={{width: `${percentage}%`}} role='progressbar' aria-valuemax={MAX} aria-valuemin={MIN} aria-valuenow={percentage.toFixed()}/>
     </div>
     
            
